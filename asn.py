8a19aa1ea8a522a0ef858d55570f1ee500b1b21f
UN5T48L3 <iletisim@un5t48l3.com>
2020-02-03 16:53:27 +0300

    sys.argv1 added

--- asn.py
+++ asn.py
@@ -1,14 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
 import subprocess, sys, re
-import os
+import os, time
 
+if len(sys.argv) < 2:
+    sys.exit("\033[91mUsage: python3 "+sys.argv[0]+" [asn list]\033[0m")
     
 url_base = 'http://ipinfo.io/'
 as_base = 'AS'
 
+print("\033[94mASN Scraper Has Been Started. Developed By UN5T48L3\033[0m")
+time.sleep(1);
+print("")
+print("\033[93mPlease wait... This will take less than a minute.\033[0m") 
+print("")
+time.sleep(1);
+
 output = open('ranges.txt', 'w')
-with open('asns.txt') as f:
+with open(sys.argv[1], "r") as f:
     lines = f.read().splitlines()
     for asn in lines:
         ASN = as_base + asn
@@ -26,4 +35,4 @@ with open('asns.txt') as f:
         os.system("cat ranges.txt | grep -v h >> ranges.lst") 
         os.system("rm -rf ranges.txt") 
         os.system("touch ranges.txt") 
-print ('All results has been saved to: ranges.lst')
+print ("\033[95mAll results has been saved to: \033[92mranges.lst\033[0m")
